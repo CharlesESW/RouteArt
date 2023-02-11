0305a5c733884e659e3724fb88a7a2030e8cf476
CarrotManMatt <matt@carrotmanmatt.com>
2023-02-11 14:04:33 +0000

    Seperated socket reciever & file reciever

---
+++ file_reciever.py
@@ -0,0 +1,12 @@
+TEST_GPS_DATA = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    ""
+]
+
+def get_location() -> str:
+    pass
+    # return next(TEST_GPS_DATA)
--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-import socket as socket_lib
 
-HOST_IP = "192.168.43.70"
-HOST_PORT = 12345
-socket = socket_lib.socket()
 
-socket.setsockopt(socket_lib.SOL_SOCKET, socket_lib.SO_REUSEADDR, 1)
-socket.bind((HOST_IP,HOST_PORT))
-socket.listen(1)
-connection, client_address = socket.accept()
+
 
 
 def main():
-    print(connection.recv(2048).decode("ascii"))
+    print()
 
 
 if __name__ == "__main__":
