6c4e8ff7da360850db0ba303e89e2bc0454ae963
CarrotManMatt <matt@carrotmanmatt.com>
2023-02-11 15:09:44 +0000

    Moved socket configs to environment variables

--- socket_receiver.py
+++ socket_receiver.py
@@ -1,13 +1,22 @@
 import socket as socket_lib
+from os import getenv
 
-HOST_IP = "10.16.171.254"
-HOST_PORT = 12345
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HOST_IP = getenv("SOCKET_HOST_IP")
+if not HOST_IP:
+    raise ValueError(f"Environment variable SOCKET_HOST_IP must be provided when using socket_receiver.")
+HOST_PORT = getenv("SOCKET_HOST_PORT")
+if not HOST_PORT:
+    raise ValueError(f"Environment variable SOCKET_HOST_PORT must be provided when using socket_receiver.")
+
 socket = socket_lib.socket()
-
 socket.setsockopt(socket_lib.SOL_SOCKET, socket_lib.SO_REUSEADDR, 1)
 socket.bind((HOST_IP,HOST_PORT))
 socket.listen(1)
 connection, client_address = socket.accept()
 
-def get_location():
+def get_location() -> str:
     return connection.recv(2048).decode("ascii")
